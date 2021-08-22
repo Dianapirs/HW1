d7da530bea61655728c88053969361b9efa8eee8
Diana <89095339+Dianapirs@users.noreply.github.com>
2021-08-22 17:21:56 +0300

    First task

---
+++ script.js
@@ -0,0 +1,11 @@
+const str = 'Привет';
+const sum = 123;
+const num = 15.8;
+const flag = true;
+const txt = 'true';
+
+console.log(str, typeof str);
+console.log(sum, typeof sum);
+console.log(num, typeof num);
+console.log(flag, typeof flag);
+console.log(txt, typeof txt);
